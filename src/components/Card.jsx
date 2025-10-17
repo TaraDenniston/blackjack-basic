36bd0f88bd06cd909c178f2bf4195217b832f97d
TaraDenniston <taradenniston@gmail.com>
2025-10-17 15:52:08 -0400

    create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import '../styles/Card.css'
+
+const Card = ({id, value, suit}) => {
+  let cardImg = `https://deckofcardsapi.com/static/img/${id}.png`;
+  let cardAlt = `${value} of ${suit}`;
+
+  return (    
+    <div className="Card">
+      <img src={cardImg} alt={cardAlt}/>
+    </div>
+  )
+}
+
+export default Card
