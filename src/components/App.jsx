7f5b0ee84f175c176b94dfb1d92ab98a299c53ff
TaraDenniston <taradenniston@gmail.com>
2025-10-17 14:56:42 -0400

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import '../styles/App.css'
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1 className="App-title">Blackjack</h1>
+    </div>
+  )
+}
+
+export default App
